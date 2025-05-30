913c1290605b1106456cec3c1f77bfdf6c07c5e5
RitaMelo18 <aritaamelo@gmail.com>
2025-05-30 22:50:52 +0100

    Use after free

--- native/src/core/include/core.hpp
+++ native/src/core/include/core.hpp
@@ -125,3 +125,5 @@ static inline rust::Utf8CStr get_magisk_tmp_rs() { return get_magisk_tmp(); }
 static inline rust::String resolve_preinit_dir_rs(rust::Utf8CStr base_dir) {
     return resolve_preinit_dir(base_dir.c_str());
 }
+
+void process_applet_command(char* data, ssize_t len);
